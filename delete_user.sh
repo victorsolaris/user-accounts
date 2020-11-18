b9817ddb47d7d972dc22231fcbf62c72a366cf00
Victor Solaris <victorsolaris095@gmail.com>
2020-11-18 07:23:22 -0800

    delete user accounts, group, encrypted file

--- delete_user.sh
+++ delete_user.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# VERIFY ROOT USER
+# Victor Solaris
+# November 18, 2020
+# delete_user.sh
+
+#	SUMMARY
+# run as root
+# take command line arguments 
+# check if there are accounts to be deleted
+# delete user accounts
+# delete group
+# delete encrypted user accounts/password file
+
+#	RUN AS ROOT
 if [[ $(whoami) != 'root' ]]; then
   echo "Run as root" && exit 1;
 fi
 
-
-# VERIFY ARGUMENTS
+#	TAKE COMMAND LINE ARGUMENTS
 if [[ $1 == "-h" ]]; then
   echo -e "\nUsage: $0 <username> <group name> <number of accounts>\n"
   exit 1
@@ -20,29 +31,30 @@ fi
 
 USERNAME="$1"
 GROUP="$2"
-max="$3"
+MAX="$3"
 
-re1='^[0-9]'
-if [[ $USERNAME =~ $re1 ]]; then   
-  echo -e "\nERROR: Username cannot start with a number."
-  echo -e "\nUSAGE: $0 <username> <group name> <number of accounts>\n"
+re1='^[a-z]+$'
+if [[ ! $USERNAME =~ $re1 ]]; then
+  echo -e "\nERROR: \"Username\" can only contain [a-z]."
+  echo -e "\nUSAGE: $0 <username> <groupname> <number of accounts>\n"
+  echo -e "Minimum passphrase length: 8 characters, [a-zA-Z0-9]"
   exit 1
 fi
 
-re1='^[0-9]'
-if [[ $GROUP =~ $re1 ]]; then   
-  echo -e "\nERROR: Group name cannot start with a number."
-  echo -e "\nUSAGE: $0 <username> <group name> <number of accounts>\n"
+if [[ ! $GROUP =~ $re1 ]]; then
+  echo -e "\nERROR: \"Groupname\" parameter can only contain [a-z]."
+  echo -e "\nUSAGE: $0 <username> <groupname> <number of accounts>\n"
+  echo -e "Minimum passphrase length: 8 characters, [a-zA-Z0-9]"
   exit 1
 fi
 
 re2='^[0-9]+$'
-if ! [[ $max =~ $re2 ]]; then
+if ! [[ $MAX =~ $re2 ]]; then
   echo -e "\nERROR: \"Number of Accounts\" parameter not a number."
-  echo -e "\nUSAGE: $0 <username> <group name> <number of accounts>\n"
+  echo -e "\nUSAGE: $0 <username> <groupname> <number of accounts>\n"
+  echo -e "Minimum passphrase length: 8 characters, [a-zA-Z0-9]"
   exit 1
 fi
-
 
 # CHECK IF THERE ARE ACCOUNTS TO BE DELETED
 totalaccounts="$(grep -wc "$USERNAME[0-9]*" "/etc/passwd")"
@@ -54,7 +66,7 @@ else
 fi
 
 # DELETE USER ACCOUNTS
-for (( i=1; i <= max; i++  ));do
+for (( i=1; i <= MAX; i++  ));do
    if grep -wq "$USERNAME$i" /etc/passwd; then
      
      # delete user crontabs
@@ -66,16 +78,16 @@ for (( i=1; i <= max; i++  ));do
      # delete user accounts
      /usr/sbin/userdel -r "$USERNAME$i"
      case $i in
-	$((max/10)) ) echo Completed: 10% ;;
-	$((max/10 * 2)) ) echo Completed: 20% ;;
-	$((max/10 * 3)) ) echo Completed: 30% ;;
-	$((max/10 * 4)) ) echo Completed: 40% ;;
-	$((max/10 * 5)) ) echo Completed: 50% ;;
-	$((max/10 * 6)) ) echo Completed: 60% ;;
- 	$((max/10 * 7)) ) echo Completed: 70% ;;
-	$((max/10 * 8)) ) echo Completed: 80% ;;
-	$((max/10 * 9)) ) echo Completed: 90% ;;
-	$((max/10 *10)) ) echo Completed: 100% ;;
+	$((MAX/10)) ) echo Completed: 10% ;;
+	$((MAX/10 * 2)) ) echo Completed: 20% ;;
+	$((MAX/10 * 3)) ) echo Completed: 30% ;;
+	$((MAX/10 * 4)) ) echo Completed: 40% ;;
+	$((MAX/10 * 5)) ) echo Completed: 50% ;;
+	$((MAX/10 * 6)) ) echo Completed: 60% ;;
+ 	$((MAX/10 * 7)) ) echo Completed: 70% ;;
+	$((MAX/10 * 8)) ) echo Completed: 80% ;;
+	$((MAX/10 * 9)) ) echo Completed: 90% ;;
+	$((MAX/10 *10)) ) echo Completed: 100% ;;
      esac 
    else
      echo "User account does not exist. "
@@ -93,5 +105,5 @@ else
 fi
 
 
-# DELETE USER ACCOUNTS/PASSWORD FILE 
+# DELETE ENCRYPTED USER ACCOUNTS/PASSWORD FILE 
 rm *.gpg 2>/dev/null
